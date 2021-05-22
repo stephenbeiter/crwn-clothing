eef7b6c87e6d97043fdfe4a90ccdaa8763b5c6ff
Stephen Beiter <stephenbeiter@gmail.com>
2021-05-22 15:54:56 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.components';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route component={HomePage} path='/' exact />
         <Route component={ShopPage} path='/shop' exact />
