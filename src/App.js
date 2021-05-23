6a0ef056d37f30021e08234bc923534da6d14cad
Stephen Beiter <stephenbeiter@gmail.com>
2021-05-22 18:08:02 -0700

    added authpage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.components';
+import HomePage from './pages/home/home.component';
+import ShopPage from './pages/shop/shop.component';
+import AuthPage from './pages/auth/auth.component';
 
 function App() {
   return (
---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './auth.styles.scss';
+
+const AuthPage = () => (
+  <div className='auth-page'>SIGN IN</div>
+);
+
+export default AuthPage;
