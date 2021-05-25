3799f112d1e5b06a3528a397f3a41acfafabb32e
Stephen Beiter <stephenbeiter@gmail.com>
2021-05-24 22:55:45 -0700

    changed signin font size

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -31,8 +31,8 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className='sign-in'>
-        <h2>I already have an account</h2>
-        <span>Sign in with your email and password</span>
+        <h1>I already have an account</h1>
+        <h2>Sign in with your email and password</h2>
 
         <form onSubmit={this.handleSubmit}>
 
