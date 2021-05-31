dcdfb0743ce05571fb2e18c058cbd14abf93104a
Stephen Beiter <stephenbeiter@gmail.com>
2021-05-31 16:53:50 -0700

    added sign out functionality

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route component={HomePage} path='/' exact />
           <Route component={ShopPage} path='/shop' exact />
