9bf525274bbe79dcbf16ee8e1ce349ee1fd81cdc
Stephen Beiter <stephenbeiter@gmail.com>
2021-06-03 11:39:25 -0700

    fixed custom button type

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children, googleButton, ...otherProps }) => (
-  <button className={`${googleButton ? 'google-button' : ''} custom-button`} {...otherProps}>
+  <button
+    className={`${googleButton ? 'google-button' : ''} custom-button`}
+    {...otherProps}
+    type='button'
+  >
     {children}
   </button>
 );
