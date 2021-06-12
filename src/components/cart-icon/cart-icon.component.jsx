021d94f5c5ef49d37257a0c0ad4cb1b9e7033c75
Stephen Beiter <stephenbeiter@gmail.com>
2021-06-11 17:07:55 -0700

    created cart-item component and added to cart-dropdown

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,15 +6,20 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
-  <div className='cart-icon' onClick={toggleCartHidden}>
+const CartIcon = ({ cartItems, toggleCartHidden }) => {
+  console.log(cartItems);
+  return (<div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
-  </div>
-);
+    <span className='item-count'>{cartItems.length}</span>
+  </div>)
+};
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -14,3 +14,10 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 };
+
+export const cartTotal = (cartItems) => {
+  const total = 0;
+  cartItems.forEach(item => {
+    total = total + item.quantity;
+  })
+}
