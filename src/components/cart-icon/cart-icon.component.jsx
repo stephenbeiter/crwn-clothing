7b88af344d79e859a03796119a98c305250c2d26
Stephen Beiter <stephenbeiter@gmail.com>
2021-06-12 04:21:08 -0700

    added reselect to memoize cart item count state

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,6 +3,7 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import './cart-icon.styles.scss';
 
@@ -13,8 +14,8 @@ const CartIcon = ({ itemCount, toggleCartHidden }) => {
   </div>)
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce((acc, item) => acc + item.quantity, 0)
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state)
 });
 
 const mapDispatchToProps = dispatch => ({
