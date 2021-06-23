197d0fd68377e1be4df24db64064385527fbd612
Stephen Beiter <stephenbeiter@gmail.com>
2021-06-22 23:21:38 -0700

    moved directory and shop data into new reducers, created respective selectors, and updated relevant components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CategoryPage from '../../components/category/category.component';
+import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({ match }) => (
-  <div className='category'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route exact path={`${match.path}/:categoryId`} component={CategoryPage} />
+  <div className='collection'>
+    <Route exact path={`${match.path}`}
+      component={CollectionsOverview} />
+    <Route exact path={`${match.path}/:collectionId`}
+      component={CollectionPage} />
   </div>
 );
 
