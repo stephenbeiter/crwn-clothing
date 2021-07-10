03e3131f4e819c89bbf3257301f8795db6055716
Stephen Beiter <stephenbeiter@gmail.com>
2021-07-09 18:34:28 -0700

    updated firebase db call to use get method

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -23,15 +23,11 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    fetch('https://firestore.googleapis.com/v1/projects/crwn-db-6ebc2/databases/(default)/documents/collections')
-      .then(response => response.json())
-      .then(collections => console.log(collections));
-
-    // collectionRef.get().then(snapshot => {
-    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-    //   updateCollections(collectionsMap);
-    //   this.setState({ loading: false });
-    // });
+    collectionRef.get().then(snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
   }
 
   render() {
